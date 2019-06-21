f6e665817c7e8489ba0a00056c219bab251d3999
ZhouYang <zhouyang@zhouyang.me>
2019-06-21 16:06:51 +0800

    update

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ from setuptools import setup
 from setuptools import find_packages
 
 setup(
-    name = "aiohttp-babel",
-    version = "0.0.6",
+    name = "dragonlib",
+    version = "0.0.1",
     packages = find_packages(),
 
     install_requires = [
